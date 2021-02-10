efb8236a91eedf42ccfd06f34809bb236c97ae53
ibrahimun <78529382+ibrahimun@users.noreply.github.com>
2021-02-10 21:56:53 +0300

    Update FirstPracticalHomework.java

--- First-Practical-Homework/src/FirstPracticalHomework.java
+++ First-Practical-Homework/src/FirstPracticalHomework.java
@@ -4,6 +4,6 @@
 public class FirstPracticalHomework 
 {
 public static void main(String[]args) {
-System.out.println("ÇáæÇíáí Úáí ÚÈÏÇááå ÇÈÑÇåíã");	
+System.out.println("Ibrahim abdulla ali al wayli");	
 }
 }
